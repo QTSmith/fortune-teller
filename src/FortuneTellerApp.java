4b1c1173de1fadd4651b84e1f3b7faa747e080a5
Quiana <qtsmith.thedev@gmail.comgit config --global user.email you@example.comgit config --global user.email qtsmith.thedev@gmail.com>
2017-05-19 15:01:02 -0400

    my terrible fortune teller app

--- src/FortuneTellerApp.java
+++ src/FortuneTellerApp.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
 
 public class FortuneTellerApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is your first name?");
+		String fName = input.nextLine();
+		System.out.println("What is your last name?");
+		String lName = input.nextLine();
+		
+		System.out.println("What is your age?");
+		String userAge = input.nextLine();
+		
+		System.out.println("Please enter the number of your birth month.");
+		String birthMonth = input.nextLine();
+		System.out.println("What is your favorite ROYGBIV color? If you do not know what ROYGBIV stands for, please type \"Help\" for a list of the ROYGBIV colors.");
+		String faveColor = input.nextLine();
+		System.out.println("Please enter the number of siblings you have");
+        String siblingNum = input.nextLine();
+        
+        //If the user’s age is an odd number, then the user will retire in ____ years if the user's age is odd, or ___ years if user's age is even.
+		if (userAge % 2 == 0){ assign int 10; }//=Even number if it's even they will 
+		else assign int 12 //=Odd number
+		//odd number
+        
+        
+        
+        
+		// fname + lname "will retiree in " + (# of years) + "with" (amount of Money) + "in the bank, a vacation home in" + (Location) and a (Mode of transportation) + "."
+		
 	}
 
 }
